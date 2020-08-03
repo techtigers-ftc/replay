831f8c695bb75ef4f892c9afd03b557179f8b5a1
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2020-08-03 17:49:01 -0400

    Added line_sensor and line_edge

---
+++ line_edge.py
@@ -0,0 +1,11 @@
+class LineEdge:
+    """Enumeration of which edge of the line the robot will follow
+    """
+
+    """Left edge of the line
+    """
+    LEFT = 0
+
+    """Right edge of the line
+    """
+    RIGHT = 1
---
+++ line_sensor.py
@@ -0,0 +1,11 @@
+class LineSensor:
+    """Enumeration of which sensor the robot uses to follow the line
+    """
+
+    """Left color sensor
+    """
+    LEFT = 0
+
+    """Right color sensor
+    """
+    RIGHT = 1
