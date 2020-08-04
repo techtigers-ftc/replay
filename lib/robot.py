e1df4aafeeb1946a17e544ae282af5f4dbe6539d
rishabh-tole <68649037+rishabh-tole@users.noreply.github.com>
2020-08-04 18:32:53 -0400

    implemented stop on color

--- lib/robot.py
+++ lib/robot.py
@@ -1,4 +1,6 @@
 from spike import MotorPair, ColorSensor
+from .colors import Color
+from .line_sensor import LineSensor
 
 class Robot:
     """ Represents the robot for the the 2021 fll season
@@ -7,17 +9,23 @@ class Robot:
     def __init__(self):
         """ Initializes the robot's motors and sensors
         """
-
         self.drive_motors = MotorPair('C', 'E')
+        self.left_color_sensor = ColorSensor('F')
+        self.right_color_sensor = ColorSensor('D')
 
-    def stop_on_color(self ,speed,color="white"):
-        """ Drives the robot until it reads white
+    def stop_on_color(self, speed, sensor, color=Color.WHITE):
+        """This functuion implements the ability to go at a certain speed 
+        and then stop on a certain color
+
+        Args:
+            speed (number): The speed at which the robot goes
+            sensor (LineSensor): Identifies the sensor to stop on a color
+            color (Color, optional): Color that the robot will stop on. Defaults to Color.WHITE.
         """
-        motor_pair.start(0,speed)
-        color_sensor = ColorSensor('F')
+        color_sensor =  self.left_color_sensor
+        if sensor == LineSensor.RIGHT:
+            color_sensor = self.right_color_sensor
+
+        self.drive_motors.start(0,speed)
         color_sensor.wait_until_color(color)
-        motor_pair.stop()
-
-        
-
-
+        self.drive_motors.stop()
