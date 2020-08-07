f30c55c1079ea745763d689ea680d0dda8ec7a7c
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2020-08-07 17:34:08 -0400

    Added follow_line() method

--- techtigers/robot.py
+++ techtigers/robot.py
@@ -12,8 +12,8 @@ class Robot:
         """ Initializes the robot's motors and sensors
         """
         self.drive_motors = MotorPair('C', 'E')
-        self.left_color_sensor = ColorSensor('F')
-        self.right_color_sensor = ColorSensor('D')
+        self.left_color = ColorSensor('F')
+        self.right_color = ColorSensor('D')
         self.hub = PrimeHub()
         self.gyro = self.hub.motion_sensor
         self.left_motor = Motor("C")
@@ -30,9 +30,9 @@ class Robot:
             sensor(LineSensor): Identifies the sensor to stop on a color
             color(Color, optional): Color that the robot will stop on. Defaults to Color.WHITE.
         """
-        color_sensor =  self.left_color_sensor
+        color_sensor =  self.left_color
         if sensor == LineSensor.RIGHT:
-            color_sensor = self.right_color_sensor
+            color_sensor = self.right_color
 
         self.drive_motors.start(0,speed)
         color_sensor.wait_until_color(color)
@@ -117,4 +117,45 @@ class Robot:
         Args:
             speed(number): The speed the motors drive at
             time(number): The amount of seconds the motors drive for
+        """
         self.drive_motors.move_tank(time,"seconds",speed,speed)
+
+    def follow_line(self, pid, speed, duration, which_sensor, which_edge):
+        """Follows the line using a color sensor.
+
+        :param pid: Uses Pid instance with parameters set beforehand
+        :type pid: Pid
+        :param speed: Speed of the Robot
+        :type speed: Number
+        :param duration: Duration of the function
+        :type duration: Number
+        :param which_sensor: The sensor the robot uses to follow the line
+        :type which_sensor: LineSensor
+        :param which_edge: Which side the white is on relative to the robot
+        :type which_edge: LineEdge
+        """
+        
+        # Inititialize values
+        pid.reset()
+
+        while pid.clock.now() < duration:
+            # Selecting which sensor to use using an Enum
+           if which_sensor == LineSensor.RIGHT:
+                error = 50 - self.right_color.get_reflected_light()
+            if which_sensor == LineSensor.LEFT:
+                error = 50 - self.left_color.get_reflected_light()
+
+            # Selecting which edge of the line to use
+            if which_edge == LineEdge.RIGHT:
+                pass
+            else:
+                error = error * -1
+
+            # Calculate steering
+            steering = pid.compute_steering(error)
+
+            # Run motors
+            self.drive_motors.start(steering, speed)
+
+        self.drive_motors.stop()
+ 
