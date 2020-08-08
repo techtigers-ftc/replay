99d78db773ecbaee8f9e8bd6bb0555a4925972b2
Aadi Kaul <aakaul06@gmail.com>
2020-08-08 14:02:42 -0400

    Finished gyro_drive

--- techtigers/robot.py
+++ techtigers/robot.py
@@ -178,3 +178,27 @@ class Robot:
                 right = True
             if left and right == True:
                 break
+    def drive(self, pid, speed, target_angle, duration):
+       # Inititialize values
+        pid.reset()
+        target_angle = target_angle % 360
+
+        while pid.clock.now() < duration:
+            # Calculate error
+            actual_angle = self.gyro.get_yaw_angle()
+            error = target_angle - actual_angle
+            error = error - (360 * int(error / 180))
+
+            # Calculate steering output
+            steering = pid.compute_steering(error)
+
+            # Drive the motors
+            self.move_tank(speed, steering)
+            self.show_image(GHOST)
+
+        # Stop motors
+        self.drive_base.stop(Stop.BRAKE)
+
+        
+
+        
