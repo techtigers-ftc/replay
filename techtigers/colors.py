067cf61bd0509d6c0141f75d4d3c304255150888
Amu Ponnapalli <amuponnapalli44@gmail.com>
2020-09-17 19:01:08 -0400

    Worked on stop on color enum

--- techtigers/colors.py
+++ techtigers/colors.py
@@ -4,32 +4,12 @@ class Color:
 
     """White color
     """
-    WHITE = range(90, 101)
+    WHITE = (90, 101)
 
     """Black color
     """
-    BLACK = range(0, 41)
-
-    """Violet color
-    """
-    VIOLET = range(0, 21)
-
-    """Blue color
-    """
-    BLUE = range(0, 21)
-
-    """Cyan color
-    """
-    CYAN = range(0, 21)
-
-    """Yellow color
-    """
-    YELLOW= range(0, 21)
-
-    """Red color
-    """
-    RED= range(0, 21)
+    BLACK = (0, 41)
 
     """Green color
     """
-    GREEN = range(80, 91)
+    GREEN = (80, 91)
