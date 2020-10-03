fb02a0c574756674f4c6f20ddaa457b5aba4827e
Amu Ponnapalli <amuponnapalli44@gmail.com>
2020-10-03 12:31:13 -0400

    Aadi: Added new classes to colors.py and tested stop_on_black

--- techtigers/colors.py
+++ techtigers/colors.py
@@ -1,15 +1,37 @@
+class ReflectedLightMatcher:
+    def __init__(self, min_value, max_value):
+        self._min_value = min_value
+        self._max_value = max_value
+
+
+    def is_match(self, color_sensor):
+        sensor_value = color_sensor.get_reflected_light() 
+        return sensor_value > self._min_value and sensor_value <= self._max_value
+
+
+class ColorMatcher:
+    def __init__(self, color_value):
+        self._color_value = color_value
+
+
+    def is_match(self, color_sensor):
+        sensor_value = color_sensor.get_color() 
+        return sensor_value == self._color_value
+
 class Color:
     """Enumeration of supported "stop on" colors
     """
 
     """White color
     """
-    WHITE = (90, 101)
+    # WHITE = (100, 101)
+    WHITE = ColorMatcher('white')
 
     """Black color
     """
-    BLACK = (0, 41)
+    BLACK = ReflectedLightMatcher(0, 35)
 
     """Green color
     """
-    GREEN = (80, 91)
+    GREEN = ReflectedLightMatcher(80, 91)
+
