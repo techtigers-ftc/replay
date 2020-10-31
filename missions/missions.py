1ce2a3b71491c87b1d06aabdf709eccaeb7f65f4
Rishi Ponnapalli <rishiponnapalli13@gmail.com>
2020-10-31 11:11:48 -0400

    Moved missions file into its own directory

--- missions/missions.py
+++ missions/missions.py
@@ -1,8 +1,5 @@
 from techtigers import Robot, Pid, LineSensor, LineEdge, Color
 
-from spike import MotorPair, ColorSensor, StatusLight, MotionSensor, Speaker, PrimeHub, Motor
-from spike.control import wait_for_seconds
-
 robot = Robot()
 drive_pid = Pid(10, 0, 2)
 reverse_drive_pid = Pid(-20, 1, 5)
